79ab6a38d057af1482e4dea48794de1d01591c15
Gilles Coomans <gilles.coomans@gmail.com>
2015-07-29 10:44:11 +0200

    add tests

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@
 
 if (typeof require !== 'undefined')
 	var chai = require("chai"),
-		c3po = require("../index");
+		c3po = require("../index.js");
 
 var expect = chai.expect;
 
@@ -19,5 +19,55 @@ describe("base", function() {
 			});
 		});
 	});
-
+	describe("define a potocol", function() {
+		c3po.protocols.foo = {
+			get: function(request, options) {
+				return "bar - " + request;
+			}
+		};
+		var res = c3po.get("foo::hello world");
+		it("should", function() {
+			expect(res).to.equal("bar - hello world");
+		});
+	});
+	describe("define a method", function() {
+		c3po.protocols.foo = {
+			zoo: function(request, options) {
+				return "zoo - " + request;
+			}
+		};
+		var res = c3po.get("foo.zoo::hello world");
+		it("should", function() {
+			expect(res).to.equal("zoo - hello world");
+		});
+	});
+	describe("define a method with args", function() {
+		c3po.protocols.foo = {
+			zoo: function(arg1, arg2, request, options) {
+				return "zoo - " + arg1 + " - " + arg2 + " " + request;
+			}
+		};
+		var res = c3po.get("foo.zoo(1, hello)::world");
+		it("should", function() {
+			expect(res).to.equal("zoo - 1 - hello world");
+		});
+	});
+	describe("use interpolation", function() {
+		c3po.interpolator = {
+			isInterpolable: function(string) {
+				return true;
+			},
+			interpolate: function(string, context) {
+				return (string === "hello { lib }") ? ("hello " + context.lib) : "something is wrong";
+			}
+		}
+		var res = c3po.get("dummy::hello { lib }", null, {
+			lib: "c3po"
+		});
+		it("should", function() {
+			expect(res).to.deep.equal({
+				dummy: "hello c3po"
+			});
+		});
+	});
 });
