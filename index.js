084ac8744cb2df764e4e78d9cb777223751c628c
Gilles Coomans <gilles.coomans@gmail.com>
2015-10-03 18:27:56 +0200

    rewrote API to provide Promise each time

--- index.js
+++ index.js
@@ -10,46 +10,85 @@
 	"use strict";
 	define("c3po", [], function() {
 		var parser = /^([\w-]+)(?:\.([\w-]+)(?:\(([^\)]*)\))?)?::([^$]*)/;
+
+		function parseRequest(request, obj) {
+			var match = parser.exec(request);
+			if (match) {
+				obj.protocol = match[1];
+				obj.method = match[2] || "get";
+				obj.args = match[3] ? match[3].split(",")
+					.map(function(arg) {
+						return arg.trim();
+					}) : null;
+				obj.pointer = match[4];
+				obj.interpolable = c3po.interpolator ? c3po.interpolator.isInterpolable(obj.pointer) : false;
+			}
+		}
+
 		var Request = function(request) {
 			this.__c3po__ = true;
 			this.original = request;
-			var protoIndex;
-			if (request[0] === '<')
+			if (request && request[0] === '<')
 				return;
-			var match = parser.exec(request);
-			if (match) {
-				this.protocol = match[1];
-				this.method = match[2] || "get";
-				this.args = match[3] ? match[3].split(",").map(function(arg) {
-					return arg.trim();
-				}) : null;
-				this.pointer = match[4];
-				this.interpolable = c3po.interpolator ? c3po.interpolator.isInterpolable(this.pointer) : false;
-			}
+			parseRequest(request, this);
 		};
+
+		function exec(protocol, self, args) {
+			if (!protocol[self.method])
+				throw new Error("there is no method named " + self.method + " in protocol " + self.protocol + "!");
+			return protocol[self.method].apply(protocol, args);
+		}
+
+		function getProtocol(name) {
+			if (typeof name === 'string') {
+				var protocol;
+				// first : look in contextualised namespace if any
+				if (c3po.fromGlocal)
+					protocol = c3po.fromGlocal(name);
+				// or look in global namespace
+				if (!protocol)
+					protocol = c3po.protocols[name];
+				if (!protocol)
+					throw new Error("no protocol found with : ", name);
+				return protocol;
+			}
+			return name;
+		}
+
+		function initialiseProtocol(protocol) {
+			return new Promise(function(resolve, reject) {
+				var promise = protocol.initialising ? protocol.initialising : protocol.init();
+				if (promise && typeof promise.then === 'function') {
+					// promised init case
+					protocol.initialising = promise.then(function() {
+						protocol.initialising = null;
+						protocol.initialised = true;
+						resolve(protocol);
+					}, reject);
+				} else {
+					protocol.initialised = true;
+					resolve(protocol);
+				}
+			});
+		}
+
 		Request.prototype = {
 			exec: function(options, context) {
 				if (!this.protocol)
-					return this.original;
+					return Promise.resolve(this.original);
 				options = options || options;
 				var protocol = c3po.protocol(this.protocol),
 					uri = (this.interpolable && c3po.interpolator && context) ? c3po.interpolator.interpolate(this.pointer, context) : this.pointer,
 					self = this,
 					args = this.args ? [].concat(this.args, uri, options) : [uri, options];
-				if (protocol && typeof protocol.then === 'function') // protocol is under init
-					return protocol.then(function(protocol) {
-					if (!protocol[self.method])
-						throw new Error("there is no method named " + self.method + " in protocol " + self.protocol + "!");
-					return protocol[self.method].apply(protocol, args);
+				return protocol.then(function(protocol) {
+					return exec(protocol, self, args);
 				});
-				if (!protocol[this.method])
-					throw new Error("there is no method named " + self.method + " in protocol " + self.protocol + "!");
-				return protocol[this.method].apply(protocol, args);
 			}
 		};
 		var c3po = {
 			Request: Request,
-			requestCache: {}, // simply set to null to avoid caching
+			requestCache: null, // simply set to {} to allow caching
 			protocols: {
 				dummy: { // dummy:: protocol for test and demo
 					get: function(url) {
@@ -62,53 +101,28 @@
 			fromGlocal: null, // to use contextualised protocols namespace. 
 			interpolator: null, // to allow request interpolation on get
 			protocol: function(name) {
-				var protocol = null;
-				if (typeof name === 'string') {
-					// first : look in contextualised namespace if any
-					if (this.fromGlocal)
-						protocol = this.fromGlocal(name);
-					// or look in global namespace
-					if (!protocol)
-						protocol = this.protocols[name];
-					if (!protocol)
-						throw new Error("no protocol found with : ", name);
-				} else
-					protocol = name;
-				// manage flattener
-				if (protocol._deep_flattener_ && !protocol._deep_flattened_)
-					return (protocol._deep_flattening_ ? protocol._deep_flattening_ : protocol.flatten())
-						.then(function() {
-							return c3po.protocol(protocol);
-						});
-				// manage ocm resolution
-				if (protocol._deep_ocm_)
-					protocol = protocol();
-				// manage initialisation if needed
-				if (protocol.init && !protocol.initialised) {
-					var promise;
-					if (protocol.initialising)
-						promise = protocol.initialising;
-					else
-						promise = protocol.init();
-					if (promise && typeof promise.then === 'function') {
-						// promised init case
-						protocol.initialising = promise;
-						return promise.then(function() {
-							protocol.initialising = null;
-							protocol.initialised = true;
-							return protocol;
-						});
-					}
-					protocol.initialised = true;
-				}
-				return protocol;
+				return new Promise(function(resolve, reject) {
+					var protocol = getProtocol(name);
+					// manage flattener
+					if (protocol._deep_flattener_ && !protocol._deep_flattened_)
+						return (protocol._deep_flattening_ ? protocol._deep_flattening_ : protocol.flatten())
+							.then(c3po.protocol)
+							.then(resolve, reject);
+					// manage ocm resolution
+					if (protocol._deep_ocm_)
+						protocol = protocol();
+					// manage initialisation if needed
+					if (protocol.init && !protocol.initialised)
+						return initialiseProtocol(protocol).then(resolve, reject);
+					resolve(protocol);
+				});
 			},
 			get: function(request, options, context) {
 				if (!request.__c3po__) {
 					if (this.requestCache && this.requestCache[request])
 						request = this.requestCache[request];
 					else {
-						request = new this.Request(request);
+						request = new Request(request);
 						if (this.requestCache)
 							this.requestCache[request.original] = request;
 					}
@@ -116,11 +130,13 @@
 				return request.exec(options, context);
 			},
 			getAll: function(requests, options, context) {
-				return requests.map(function(request) {
+				return Promise.all(requests.map(function(request) {
 					return c3po.get(request, options, context);
-				});
+				}));
 			}
 		};
+
+		// module.exports = c3po;
 		return c3po;
 	});
 })(typeof define !== 'undefined' ? define : function(id, deps, factory) { // AMD/RequireJS format if available
