b82cab3ef8f30c3dd3ef1a5270e044c7d151d955
Gilles Coomans <gilles.coomans@gmail.com>
2016-04-26 17:55:25 +0200

    add Loopback Model's client

---
+++ client/loopback-client.js
@@ -0,0 +1,25 @@
+/**
+ * Simple loopback.io model's  client
+ */
+
+var qs = require('qs'); // query string parser used by loopback
+
+function LoopbackClient(app, modelName) {
+	this.Model = app.models[modelName];
+	if (!this.Model)
+		throw new Error('LoopbackClient : no model found with : ' + modelName);
+	this.modelName = modelName;
+}
+
+LoopbackClient.prototype = {
+	get: function(query, opt) {
+		if (query[0] === '?')
+			return this.Model.find(qs.parse(query.substring(1)));
+		return this.Model.findById(query);
+	},
+	first: function(query) {
+		return this.Model.findOne(qs.parse(query[0] === '?' ? query.substring(1) : query));
+	}
+};
+
+module.exports = LoopbackClient;
