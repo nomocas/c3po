011609f37416897e2768750aca9c7cd6e61c41dd
Gilles Coomans <gilles.coomans@gmail.com>
2016-05-19 23:34:17 +0200

    add missing opt

--- client/axios-client.js
+++ client/axios-client.js
@@ -1,33 +1,68 @@
 var axios = require('axios');
 
-function AxiosClient(baseURI, defaultObj) {
-	this.baseURI = baseURI;
-	this.defaultObject = defaultObj;
+function AxiosClient(opt) {
+	opt = opt || {};
+	this.baseURI = opt.baseURI;
+	this.defaultObject = opt.defaultObj;
+	if (opt.parser)
+		this.parser = function(data, req) {
+			try {
+				return this.parser(data, req);
+			} catch (e) {
+				console.error('AxiosClient : error while parsing ! ', e);
+				return null;
+			}
+		};
+	if (opt.useCache)
+		this.cache = {};
 }
 
 AxiosClient.prototype = {
-	get: function(query, opt) {
-		return axios.get(this.baseURI + query)
+	get: function(req, opt) {
+		if (this.cache && this.cache[req])
+			return this.cache[req];
+		var self = this;
+		return axios.get(this.baseURI + req)
 			.then(function(s) {
+				if (self.parser)
+					s.data = self.parser(s.data, req);
+				if (self.cache)
+					self.cache[req] = s.data;
 				return s.data;
 			});
 	},
 	del: function(id, opt) {
-		return axios.delete(this.baseURI + id)
+		var self = this,
+			req = this.baseURI + id;
+		return axios.delete(req)
 			.then(function(s) {
+				if (self.cache && self.cache[req])
+					delete self.cache[req];
 				return s.data;
 			});
 	},
 	post: function(data, opt) {
+		var self = this;
 		return axios.post(this.baseURI, data)
 			.then(function(s) {
 				data.id = s.data.id;
+				var req = self.baseURI + s.data.id;
+				if (self.parser)
+					s.data = self.parser(s.data, req);
+				if (self.cache)
+					self.cache[req] = s.data;
 				return s.data;
 			});
 	},
 	put: function(data, opt) {
-		return axios.put(this.baseURI + data.id, data)
+		var self = this,
+			req = this.baseURI + data.id;
+		return axios.put(req, data)
 			.then(function(s) {
+				if (self.parser)
+					s.data = self.parser(s.data, req);
+				if (self.cache)
+					self.cache[req] = s.data;
 				return s.data;
 			});
 	},
