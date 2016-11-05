014eec7d04d89eeedbe49b33bd3e9da55f05dcb1
Gilles Coomans <gilles.coomans@gmail.com>
2016-11-05 01:47:21 +0100

    debug axios client

--- client/axios-client.js
+++ client/axios-client.js
@@ -8,7 +8,7 @@ function rethrowError(e) {
 function AxiosClient(opt) {
 	opt = opt || {};
 	this.baseURI = opt.baseURI;
-	this.defaultObject = opt.defaultObj ||  {};
+	this.defaultObj = opt.default ||  {};
 	if (opt.parser)
 		this.parser = function(data, req) {
 			if (typeof data !== "string")
@@ -30,7 +30,9 @@ AxiosClient.prototype = {
 			return this.cache[req];
 		var self = this,
 			uri = this.baseURI + req;
-		return axios.get(uri)
+		return axios.get(uri, {
+				headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
+			})
 			.then(function(s) {
 				if (self.parser)
 					s.data = self.parser(s.data, req);
@@ -48,7 +50,9 @@ AxiosClient.prototype = {
 	del: function(id, opt) {
 		var self = this,
 			req = this.baseURI + id;
-		return axios.delete(req)
+		return axios.delete(req, {
+				headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
+			})
 			.then(function(s) {
 				if (self.cache && self.cache[req])
 					delete self.cache[req];
@@ -63,7 +67,9 @@ AxiosClient.prototype = {
 	},
 	post: function(data, opt) {
 		var self = this;
-		return axios.post(this.baseURI, data)
+		return axios.post(this.baseURI, data, {
+				headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
+			})
 			.then(function(s) {
 				data.id = s.data.id;
 				var req = self.baseURI + s.data.id;
@@ -88,6 +94,8 @@ AxiosClient.prototype = {
 				id: id,
 				path: path,
 				value: value
+			}, {
+				headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
 			})
 			.then(function(s) {
 				return s.data;
@@ -105,7 +113,9 @@ AxiosClient.prototype = {
 	put: function(data, opt) {
 		var self = this,
 			req = this.baseURI + data.id;
-		return axios.put(req, data)
+		return axios.put(req, data, {
+				headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
+			})
 			.then(function(s) {
 				if (self.parser)
 					s.data = self.parser(s.data, req);
@@ -122,11 +132,13 @@ AxiosClient.prototype = {
 			});
 	},
 	default: function() {
-		return y.utils.copy(this.defaultObject);
+		return y.utils.copy(typeof this.defaultObj === 'function' ? this.defaultObj() : this.defaultObj);
 	},
 	first: function(filter, opt) {
 		var uri = this.baseURI + 'findOne/' + (filter || '');
-		return axios.get(uri)
+		return axios.get(uri, {
+				headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
+			})
 			.then(function(s) {
 				return s.data;
 			})
@@ -139,7 +151,9 @@ AxiosClient.prototype = {
 	},
 	remote: function(method, data, opt) {
 		var uri = this.baseURI + method;
-		return axios.post(uri, data)
+		return axios.post(uri, data, {
+				headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
+			})
 			.then(function(s) {
 				return s.data;
 			})
