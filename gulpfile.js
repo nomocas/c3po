60e8e58b2fb7324e0d4a4b939e562023776b10ad
Gilles Coomans <gilles.coomans@gmail.com>
2017-07-10 11:22:05 +0200

    lint(gulpfile): clean and lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,37 @@
+'use strict';
+
 var gulp = require('gulp'),
-    gls = require('gulp-live-server'),
-    uglify = require('gulp-uglifyjs'),
-    rename = require("gulp-rename");
-//___________________________________________________
+	gls = require('gulp-live-server'),
+	uglify = require('gulp-uglifyjs'),
+	rename = require('gulp-rename');
+// ___________________________________________________
 
 gulp.task('default', ['lint', 'uglify']);
 gulp.task('lint', ['jslint']);
 
-//___________________________________________________
+// ___________________________________________________
 // npm i --save-dev gulp-jshint jshint-stylish
 var jshint = require('gulp-jshint'),
-    stylish = require('jshint-stylish');
+	stylish = require('jshint-stylish');
 
 gulp.task('jslint', function() {
-    gulp.src('./index.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter(stylish));
+	gulp.src('./index.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter(stylish));
 });
-//___________________________________________________
+// ___________________________________________________
 gulp.task('serve-test', function() {
-    var server = gls.static(".", 8287);
-    server.start();
-    //live reload changed resource(s) 
-    gulp.watch(['index.js', 'test/**/*.js'], server.notify);
+	var server = gls.static('.', 8287);
+	server.start();
+	// live reload changed resource(s)
+	gulp.watch(['index.js', 'test/**/*.js'], server.notify);
 });
-//___________________________________________________
+// ___________________________________________________
 gulp.task('uglify', function() {
-    gulp.src('index.js')
-        .pipe(uglify())
-        .pipe(rename('c3po.min.js'))
-        .pipe(gulp.dest('dist'))
+	gulp.src('index.js')
+		.pipe(uglify())
+		.pipe(rename('c3po.min.js'))
+		.pipe(gulp.dest('dist'));
 });
 
 
@@ -38,24 +40,21 @@ var istanbul = require('gulp-istanbul');
 var mocha = require('gulp-mocha');
 
 gulp.task('test', function(cb) {
-    gulp.src(['index.js'])
-        .pipe(istanbul()) // Covering files
-        .pipe(istanbul.hookRequire()) // Force `require` to return covered files
-        .on('finish', function() {
-            gulp.src(['test/*.js'])
-                .pipe(mocha())
-                .pipe(istanbul.writeReports()) // Creating the reports after tests ran
-                /*.pipe(istanbul.enforceThresholds({
-                    thresholds: {
-                        global: 90
-                    }
-                })) */ // Enforce a coverage of at least 90%
-                .on('end', cb);
-        });
+	gulp.src(['index.js'])
+		.pipe(istanbul()) // Covering files
+		.pipe(istanbul.hookRequire()) // Force `require` to return covered files
+		.on('finish', function() {
+			gulp.src(['test/*.js'])
+				.pipe(mocha())
+				.pipe(istanbul.writeReports()) // Creating the reports after tests ran
+				/* .pipe(istanbul.enforceThresholds({
+					thresholds: {
+						global: 90
+					}
+				})) */ // Enforce a coverage of at least 90%
+				.on('end', cb);
+		});
 });
-
-
-'use strict';
 
 // npm i --save-dev browserify vinyl-source-str vinyl-buffer gulp-util gulp-uglify gulp-sourcemaps
 
@@ -63,28 +62,27 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var gutil = require('gulp-util');
-var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 // var reactify = require('reactify');
 
 gulp.task('build', function() {
-    // set up the browserify instance on a task basis
-    var b = browserify({
-        // entries: './index.js',
-        debug: true
-            // defining transforms here will avoid crashing your stream
-            // , transform: [reactify]
-    });
+	// set up the browserify instance on a task basis
+	var b = browserify({
+		// entries: './index.js',
+		debug: true
+			// defining transforms here will avoid crashing your stream
+			// , transform: [reactify]
+	});
 
-    return b.bundle()
-        .pipe(source('index.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({
-            loadMaps: true
-        }))
-        // Add transformation tasks to the pipeline here.
-        .pipe(uglify())
-        .on('error', gutil.log)
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./dist/'));
+	return b.bundle()
+		.pipe(source('index.js'))
+		.pipe(buffer())
+		.pipe(sourcemaps.init({
+			loadMaps: true
+		}))
+		// Add transformation tasks to the pipeline here.
+		.pipe(uglify())
+		.on('error', gutil.log)
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('./dist/'));
 });
