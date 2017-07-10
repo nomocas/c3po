1d42013440f89113bdecc328cc40db620fa64813
Gilles Coomans <gilles.coomans@gmail.com>
2017-07-10 11:27:05 +0200

    fix(axios-client): add better error handling and constructor options

--- client/axios-client.js
+++ client/axios-client.js
@@ -1,17 +1,15 @@
+/* eslint no-console: 0 */
 var axios = require('axios');
-require('nomocas-utils/lib/promise-log');
-
-function rethrowError(e) {
-	throw e.data.error;
-}
 
 function AxiosClient(opt) {
 	opt = opt || {};
-	this.baseURI = opt.baseURI;
-	this.defaultObj = opt.default ||  {};
+	this.defaultObj = opt.default || {};
+	for (var i in opt)
+		if (i !== 'default' && i !== 'parser' && i !== 'useCache')
+			this[i] = opt[i];
 	if (opt.parser)
 		this.parser = function(data, req) {
-			if (typeof data !== "string")
+			if (typeof data !== 'string')
 				return data;
 			try {
 				return opt.parser(data, req);
@@ -22,17 +20,32 @@ function AxiosClient(opt) {
 		};
 	if (opt.useCache)
 		this.cache = {};
+	this.handleError = this.handleError.bind(this);
 }
 
 AxiosClient.prototype = {
-	get: function(req, opt) {
+	handleError: function(e) {
+		if (e.response) {
+			console.error('Axios : ', e.response.status, e.response.data);
+			// console.error('Axios : ', e.response.status, e.response.data.error.message);
+			if (AxiosClient.debug)
+				console.log(e.response.data.error.stack);
+			if (this._errors && this._errors[String(e.response.status)])
+				return this._errors[e.response.status](e.response);
+		} else if (e.status) {
+			if (this._errors && this._errors[e.status])
+				return this._errors[e.status](e);
+		}
+		throw e;
+	},
+	get: function(req /* , opt*/) {
 		if (this.cache && this.cache[req])
 			return this.cache[req];
 		var self = this,
 			uri = this.baseURI + req;
 		return axios.get(uri, {
-				headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
-			})
+			headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
+		})
 			.then(function(s) {
 				if (self.parser)
 					s.data = self.parser(s.data, req);
@@ -40,36 +53,27 @@ AxiosClient.prototype = {
 					self.cache[req] = s.data;
 				return s.data;
 			})
-			.catch(rethrowError)
-			.addToError({
-				level: 'AxiosClient',
-				method: 'get',
-				uri: uri
-			});
+			.catch(this.handleError);
 	},
-	del: function(id, opt) {
+	del: function(id /* , opt */) {
 		var self = this,
 			req = this.baseURI + id;
 		return axios.delete(req, {
-				headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
-			})
+			headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
+		})
 			.then(function(s) {
 				if (self.cache && self.cache[req])
 					delete self.cache[req];
 				return s.data;
 			})
-			.catch(rethrowError)
-			.addToError({
-				level: 'AxiosClient',
-				method: 'del',
-				uri: req
-			});
+			.catch(this.handleError);
 	},
 	post: function(data, opt) {
 		var self = this;
-		return axios.post(this.baseURI, data, {
-				headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
-			})
+		opt = opt || {};
+		return axios.post(this.baseURI + (opt.path ? opt.path : ''), data, {
+			headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
+		})
 			.then(function(s) {
 				data.id = s.data.id;
 				var req = self.baseURI + s.data.id;
@@ -79,43 +83,28 @@ AxiosClient.prototype = {
 					self.cache[req] = s.data;
 				return s.data;
 			})
-			.catch(rethrowError)
-			.addToError({
-				level: 'AxiosClient',
-				method: 'post',
-				uri: this.baseURI,
-				data: data
-			});
+			.catch(this.handleError);
 	},
-	patch: function(id, value, path, opt) {
-		var self = this,
-			uri = this.baseURI + 'patch';
+	patch: function(id, value, path /* , opt*/) {
+		var uri = this.baseURI + 'patch';
 		return axios.post(uri, {
-				id: id,
-				path: path,
-				value: value
-			}, {
-				headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
-			})
+			id: id,
+			path: path,
+			value: value
+		}, {
+			headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
+		})
 			.then(function(s) {
 				return s.data;
 			})
-			.catch(rethrowError)
-			.addToError({
-				level: 'AxiosClient',
-				method: 'patch',
-				uri: uri,
-				id: id,
-				value: value,
-				path: path
-			});
+			.catch(this.handleError);
 	},
-	put: function(data, opt) {
+	put: function(data /* , opt*/) {
 		var self = this,
 			req = this.baseURI + data.id;
 		return axios.put(req, data, {
-				headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
-			})
+			headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
+		})
 			.then(function(s) {
 				if (self.parser)
 					s.data = self.parser(s.data, req);
@@ -123,49 +112,38 @@ AxiosClient.prototype = {
 					self.cache[req] = s.data;
 				return s.data;
 			})
-			.catch(rethrowError)
-			.addToError({
-				level: 'AxiosClient',
-				method: 'put',
-				uri: req,
-				data: data
-			});
+			.catch(this.handleError);
 	},
 	default: function() {
-		return y.utils.copy(typeof this.defaultObj === 'function' ? this.defaultObj() : this.defaultObj);
+		return JSON.parse(JSON.stringify(typeof this.defaultObj === 'function' ? this.defaultObj() : this.defaultObj));
 	},
-	first: function(filter, opt) {
+	first: function(filter /* , opt*/) {
 		var uri = this.baseURI + 'findOne/' + (filter || '');
 		return axios.get(uri, {
-				headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
-			})
+			headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
+		})
 			.then(function(s) {
 				return s.data;
 			})
-			.catch(rethrowError)
-			.addToError({
-				level: 'AxiosClient',
-				method: 'first',
-				uri: uri
-			});
+			.catch(this.handleError);
 	},
-	remote: function(method, data, opt) {
+	remote: function(method, data /* , opt*/) {
 		var uri = this.baseURI + method;
 		return axios.post(uri, data, {
-				headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
-			})
+			headers: AxiosClient.getHeaders ? AxiosClient.getHeaders() : null
+		})
 			.then(function(s) {
 				return s.data;
 			})
-			.catch(rethrowError)
-			.addToError({
-				level: 'AxiosClient',
-				method: 'remote',
-				uri: uri,
-				remoteMethod: method,
-				data: data
-			});
+			.catch(this.handleError);
 	}
 };
+
+AxiosClient.prototype.delete = AxiosClient.prototype.del;
+AxiosClient.prototype.update = AxiosClient.prototype.put;
+AxiosClient.prototype.create = AxiosClient.prototype.post;
+AxiosClient.prototype.findOne = AxiosClient.prototype.first;
+AxiosClient.prototype.find = AxiosClient.prototype.get;
+
 
 module.exports = AxiosClient;
